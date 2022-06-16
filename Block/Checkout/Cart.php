563115d7574f5f4d42f95250f7d51137fc4a47db
Martin Sternelius <martin.sternelius@nordicwebteam.se>
2022-06-16 11:48:45 +0200

    [SC-146] Quantity increment / decrement cart item buttons now support products' configured qty increments

--- Block/Checkout/Cart.php
+++ Block/Checkout/Cart.php
@@ -1,6 +1,9 @@
 <?php
 namespace Svea\Checkout\Block\Checkout;
 
+use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
+use Magento\InventoryConfigurationApi\Api\GetStockItemConfigurationInterface;
+use Magento\Quote\Model\Quote\Item;
 
 class Cart extends \Magento\Checkout\Block\Cart\Totals
 {
@@ -39,6 +42,15 @@ class Cart extends \Magento\Checkout\Block\Cart\Totals
      */
     protected $helper;
 
+    /**
+     * @var DefaultStockProviderInterface
+     */
+    protected DefaultStockProviderInterface $defaultStock;
+
+    /**
+     * @var GetStockItemConfigurationInterface
+     */
+    protected GetStockItemConfigurationInterface $getStockConfig;
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
@@ -46,10 +58,14 @@ class Cart extends \Magento\Checkout\Block\Cart\Totals
         \Magento\Checkout\Model\Session $checkoutSession,
         \Magento\Sales\Model\Config $salesConfig,
         \Svea\Checkout\Helper\Data $helper,
+        DefaultStockProviderInterface $defaultStock,
+        GetStockItemConfigurationInterface $getStockConfig,
         array $layoutProcessors = [],
         array $data = []
     ) {
         $this->helper = $helper;
+        $this->defaultStock = $defaultStock;
+        $this->getStockConfig = $getStockConfig;
         parent::__construct($context, $customerSession, $checkoutSession,$salesConfig, $layoutProcessors,$data);
     }
 
@@ -58,6 +74,23 @@ class Cart extends \Magento\Checkout\Block\Cart\Totals
 
         return $this->helper->showCouponLayout();
     }
+
+    /**
+     * Get qty increment configuration and set on item object
+     * Then get item row html
+     *
+     * @param \Magento\Quote\Model\Quote\Item $item
+     * @return  string
+     */
+    public function getItemHtml(Item $item)
+    {
+        $qtyIncrements = 1;
+        try {
+            $stockConfig = $this->getStockConfig->execute($item->getSku(), $this->defaultStock->getId());
+            $qtyIncrements = ($stockConfig->isEnableQtyIncrements()) ? $stockConfig->getQtyIncrements() : 1;
+        } finally {
+            $item->setData('qty_increments', $qtyIncrements);
+            return parent::getItemHtml($item);
+        }
+    }
 }
-
-
