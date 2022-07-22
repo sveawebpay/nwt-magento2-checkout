0a43de8f1a4f7448a14de8e25ed12a5c52e84206
Alex Mazalu <george@nordicwebteam.se>
2022-07-22 18:52:03 +0300

    Use product stock item instead of magento/inventory*

--- Service/GetQtyIncrementConfig.php
+++ Service/GetQtyIncrementConfig.php
@@ -3,8 +3,6 @@
 namespace Svea\Checkout\Service;
 
 use Magento\Catalog\Api\Data\ProductInterface;
-use Magento\InventoryCatalogApi\Api\DefaultStockProviderInterface;
-use Magento\InventoryConfigurationApi\Api\GetStockItemConfigurationInterface;
 use Svea\Checkout\Api\Data\QtyIncrementConfigInterface;
 use Svea\Checkout\Api\GetQtyIncrementConfigInterface;
 use Svea\Checkout\Model\QtyIncrementConfigFactory;
@@ -12,36 +10,24 @@ use Svea\Checkout\Model\QtyIncrementConfigFactory;
 class GetQtyIncrementConfig implements GetQtyIncrementConfigInterface
 {
     /**
-     * @var DefaultStockProviderInterface
-     */
-    private $defaultStock;
-
-    /**
-     * @var GetStockItemConfigurationInterface
-     */
-    private $getStockConfig;
-
-    /**
      * @var QtyIncrementConfigFactory
      */
     private $qtyIncrementFactory;
 
     public function __construct(
-        DefaultStockProviderInterface $defaultStock,
-        GetStockItemConfigurationInterface $getStockConfig,
         QtyIncrementConfigFactory $qtyIncrementFactory
     ) {
-        $this->defaultStock = $defaultStock;
-        $this->getStockConfig = $getStockConfig;
         $this->qtyIncrementFactory = $qtyIncrementFactory;
     }
 
     public function execute(ProductInterface $product): QtyIncrementConfigInterface
     {
-        $stockConfig = $this->getStockConfig->execute($product->getSku(), $this->defaultStock->getId());
+        // MSI disabled - can't use Magento\Inventory*
+        $qtyIncrementFromStockItem = $product->getStockItem()->getData('qty_increments');
+
         $qtyIncrement = $this->qtyIncrementFactory->create();
-        $qtyIncrement->setEnableQtyIncrements($stockConfig->isEnableQtyIncrements());
-        $qtyIncrement->setQtyIncrements($stockConfig->getQtyIncrements());
+        $qtyIncrement->setEnableQtyIncrements(true);
+        $qtyIncrement->setQtyIncrements($qtyIncrementFromStockItem);
         return $qtyIncrement;
     }
 }
