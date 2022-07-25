97e75756fca8e0d25ed94b658c25b06840d801ce
martin.sternelius <martin.sternelius@nordicwebteam.se>
2022-07-25 14:36:58 +0000

    Merged in SC-146 (pull request #36)
    [SC-146] Fixes stock item loading
    
    * [SC-146] Fixes loading of stock item for getting qty increment config. Removes composer suggest for Svea_NoMSI.
    
    
    Approved-by: Alexandru-Georgian Mazalu

--- Service/GetQtyIncrementConfig.php
+++ Service/GetQtyIncrementConfig.php
@@ -3,6 +3,7 @@
 namespace Svea\Checkout\Service;
 
 use Magento\Catalog\Api\Data\ProductInterface;
+use Magento\CatalogInventory\Api\StockRegistryInterface;
 use Svea\Checkout\Api\Data\QtyIncrementConfigInterface;
 use Svea\Checkout\Api\GetQtyIncrementConfigInterface;
 use Svea\Checkout\Model\QtyIncrementConfigFactory;
@@ -10,24 +11,30 @@ use Svea\Checkout\Model\QtyIncrementConfigFactory;
 class GetQtyIncrementConfig implements GetQtyIncrementConfigInterface
 {
     /**
+     * @var StockRegistryInterface
+     */
+    private $stockRegistry;
+
+    /**
      * @var QtyIncrementConfigFactory
      */
     private $qtyIncrementFactory;
 
     public function __construct(
+        StockRegistryInterface $stockRegistry,
         QtyIncrementConfigFactory $qtyIncrementFactory
     ) {
+        $this->stockRegistry = $stockRegistry;
         $this->qtyIncrementFactory = $qtyIncrementFactory;
     }
 
     public function execute(ProductInterface $product): QtyIncrementConfigInterface
     {
-        // MSI disabled - can't use Magento\Inventory*
-        $qtyIncrementFromStockItem = $product->getStockItem()->getData('qty_increments');
+        $stockItem = $this->stockRegistry->getStockItem($product->getId());
 
         $qtyIncrement = $this->qtyIncrementFactory->create();
-        $qtyIncrement->setEnableQtyIncrements(true);
-        $qtyIncrement->setQtyIncrements($qtyIncrementFromStockItem);
+        $qtyIncrement->setEnableQtyIncrements($stockItem->getEnableQtyIncrements());
+        $qtyIncrement->setQtyIncrements($stockItem->getQtyIncrements());
         return $qtyIncrement;
     }
 }
