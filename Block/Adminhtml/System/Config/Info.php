afc5daa0d888e09ae8de324221668cc4bb35b96f
Martin Sternelius <martin.sternelius@nordicwebteam.se>
2024-02-07 17:52:36 +0100

    [SC-217] Displays composer version under logo in module config

--- Block/Adminhtml/System/Config/Info.php
+++ Block/Adminhtml/System/Config/Info.php
@@ -25,4 +25,21 @@ class Info extends Template implements RendererInterface
         $elementOriginalData = $element->getOriginalData();
         return $this->toHtml();
     }
+
+    /**
+     * @return string
+     */
+    public function getComposerVersion(): string
+    {
+        return $this->getLayoutHelper()->getComposerVersion();
+    }
+
+    /**
+     * @return \Svea\Checkout\Helper\Adminhtml\Layout
+     */
+    private function getLayoutHelper(): \Svea\Checkout\Helper\Adminhtml\Layout
+    {
+        // Set in di.xml
+        return $this->getData('layout_helper');
+    }
 }
